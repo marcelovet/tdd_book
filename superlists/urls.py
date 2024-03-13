8c709ccc44d562b783f0f2effd9f7de65872e1bf
marcelo paiva <marcelo_thelin@hotmail.com>
2024-03-13 14:09:39 -0300

    new url for future multiple users, view and template to display lists

--- superlists/urls.py
+++ superlists/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 
-from lists.views import home_page
+from lists import views
 
 urlpatterns = [
-    path('', home_page, name='home'),
+    path('', views.home_page, name='home'),
+    path('lists/the-only-list-in-the-world/',
+         views.view_list, name='view_list'),
 ]
